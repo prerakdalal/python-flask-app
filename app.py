55fd354dd19b738636cbfb25c2d50c60332dc887
prerak dalal <prerakdalal@gmail.com>
2025-01-15 22:55:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'Well done! Testing the Pipeline Execution -3rd time'
+        return 'Well done! Testing the Pipeline Execution -4th time'
 
     return app
 if __name__ == '__main__':
